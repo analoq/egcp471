4679a4f97edd2c5ad6920eafb81cab889c4ce9d2
a. p. matthews <i@apmatthe.ws>
2019-05-13 10:01:21 -0700

    move comments into README

---
+++ README.md
@@ -0,0 +1,19 @@
+Python code handles:
+- Forward Kinematics
+- Inverse Kinematics
+- Position Control Loop (proportional)
+- Serial communication
+- 2D peak finding?
+
+Arduino code handles:
+- Sensor reading
+- Motor control
+- Serial communication
+
+Sample of Inverse Kinematics:
+  a_measured: (65.01158903746261, -97.38577458793476)
+  a_expected: (67.5, -98)
+  a_error: 3.7%, 0.92% 
+  p_measured: (20.784, 17.314)
+  p_expected: (21.59, 16.51)
+  p_error: 3.75%, 4.85%
--- backhoe.py
+++ backhoe.py
@@ -1,25 +1,3 @@
-"""
-Python:
-- Forward Kinematics
-- Inverse Kinematics
-- Velocity Control Loop (constant)
-- Position Control Loop (proportional)
-- Serial communication
-- 2D peak finding?
-
-Arduino:
-- Sensor reading
-- Motor control
-- Serial communication
-
-Example:
-a_measured: (65.01158903746261, -97.38577458793476)
-a_expected: (67.5, -98)
-a_error: 3.7%, 0.92% 
-p_measured: (20.784, 17.314)
-p_expected: (21.59, 16.51)
-p_error: 3.75%, 4.85%
-"""
 import math
 import time
 
