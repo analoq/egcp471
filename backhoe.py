67b7f79176ac2a8d30a86b8b53ae65299cbba86a
a. p. matthews <i@apmatthe.ws>
2019-05-13 09:57:41 -0700

    arduino code initial commit.  python add code for sensor reading and motor control

--- backhoe.py
+++ backhoe.py
@@ -20,10 +20,15 @@ p_measured: (20.784, 17.314)
 p_expected: (21.59, 16.51)
 p_error: 3.75%, 4.85%
 """
+import math
+import time
 
 from scipy.optimize import minimize, Bounds
 import numpy as np
-import math
+import serial
+
+COM_PATH = '/dev/cu.usbmodem1411'
+GRAVITY = 9.807
 
 def deg2rad(deg):
     return math.pi/180.0 * deg
@@ -58,10 +63,86 @@ def ikine(theta_boom, theta_stick, x, z):
                   # options={'xtol': 1e-8, 'disp': True})
     return res.x
 
+def get_boom_angle():
+    com_port.write([0x11, 0x00, 0x00])
+    value = float(com_port.readline())
+    ratio = value/GRAVITY
+    if ratio > 1.0:
+        ratio = 1.0
+    elif ratio < -1.0:
+        ratio = -1.0
+    return -math.asin(ratio)
+
+def get_stick_angle():
+    boom_angle = get_boom_angle()
+    com_port.write([0x12, 0x00, 0x00])
+    value = float(com_port.readline())
+    ratio = value/GRAVITY
+    if ratio > 1.0:
+        ratio = 1.0
+    elif ratio < -1.0:
+        ratio = -1.0
+    abs_stick_angle = -math.asin(ratio)
+    return abs_stick_angle - boom_angle
+
+def fire(joint, delta_theta):
+    DIR_POS = 0x01
+    DIR_NEG = 0x00
+    joints = {
+        'boom': {'address': 0x01,
+                 'magnitude': 0xA0,
+                 'pos_secrad': 1.0 / deg2rad(31.654),
+                 'neg_secrad': 1.0 / deg2rad(41.607)},
+        'stick': {'address': 0x02,
+                  'magnitude': 0xB0,
+                  'pos_secrad': 1.0 / deg2rad(19.95),
+                  'neg_secrad': 1.0 / deg2rad(22.22)},
+        'bucket': {'address': 0x03,
+                   'magnitude': 0xFF,
+                   'pos_secrad': 1.0 / deg2rad(61.538),
+                   'neg_secrad': 1.0 / deg2rad(88.495)},
+        'house': {'address': 0x04,
+                  'magnitude': 0xA0,
+                  'pos_secrad': 1.0 / deg2rad(47.368),
+                  'neg_secrad': 1.0 / deg2rad(47.368)},
+    }
+
+    if delta_theta > 0:
+        com_port.write([joints[joint]['address'], DIR_POS, joints[joint]['magnitude']])
+        time.sleep(joints[joint]['pos_secrad'] * delta_theta)
+    else:
+        com_port.write([joints[joint]['address'], DIR_NEG, joints[joint]['magnitude']])
+        time.sleep(joints[joint]['neg_secrad'] * -delta_theta)
+    com_port.write([joints[joint]['address'], DIR_NEG, 0x00])
+
+def control(x, y):
+    theta_boom = get_boom_angle()
+    theta_stick = get_stick_angle()
+    dest_theta_boom, dest_theta_stick = ikine(theta_boom, theta_stick, x, y)
+
+    # move boom
+    delta_theta_boom = dest_theta_boom - theta_boom
+    print rad2deg(delta_theta_boom)
+    fire('boom', delta_theta_boom)
+
+    # move stick
+    delta_theta_stick = dest_theta_stick - theta_stick
+    print rad2deg(delta_theta_stick)
+    fire('stick', delta_theta_stick)
+
+def pickup():
+    fire('bucket', deg2rad(-120))
+
+def dropoff():
+    fire('bucket', deg2rad(80))
 
 if __name__ == "__main__":
-    #print fkine(deg2rad(-45), deg2rad(-45))
-    x = 21.59
-    z = 16.51
-    theta_boom, theta_stick = ikine(deg2rad(0), deg2rad(0), x, z)
-    print (rad2deg(theta_boom), rad2deg(theta_stick))
+    com_port = serial.Serial(COM_PATH, timeout=60)
+    time.sleep(1.0)
+    #control(30, 20)
+    #dropoff()
+    #time.sleep(1.0)
+    #control(15, 0)
+    #pickup()
+    #fire('house', deg2rad(-30))
+    com_port.close()
---
+++ backhoe/backhoe.ino
@@ -0,0 +1,110 @@
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+
+#define BOOM_1    2
+#define BOOM_2    3
+#define STICK_1   4
+#define STICK_2   5
+#define BUCKET_1  6
+#define BUCKET_2  7
+#define HOUSE_1   8
+#define HOUSE_2   9
+
+Adafruit_MMA8451 stick_mma = Adafruit_MMA8451();
+Adafruit_MMA8451 boom_mma = Adafruit_MMA8451();
+
+void setup()
+{
+  Serial.begin(9600);
+  while(!Serial)
+  {
+      delay(10);
+  }
+  
+  pinMode(BOOM_1, OUTPUT);
+  pinMode(BOOM_2, OUTPUT);
+  pinMode(STICK_1, OUTPUT);
+  pinMode(STICK_2, OUTPUT);
+  pinMode(BUCKET_1, OUTPUT);
+  pinMode(BUCKET_2, OUTPUT);
+  pinMode(HOUSE_1, OUTPUT);
+  pinMode(HOUSE_2, OUTPUT);
+
+  if (!boom_mma.begin(0x1C))  // use alternate address
+  {
+    Serial.println("Couldnt start boom accelerometer");
+    while (1);
+  }
+
+  if (!stick_mma.begin(0x1D))  // use default address
+  {
+    Serial.println("Couldnt start stick accelerometer");
+    while (1);
+  }
+}
+
+void fire(unsigned char port1, unsigned char port2, unsigned char dir, unsigned char mag)
+{
+  if (dir)
+  {
+    analogWrite(port1, mag);
+    analogWrite(port2, 0);
+  }
+  else
+  {
+    analogWrite(port2, mag);
+    analogWrite(port1, 0);
+  }
+}
+
+sensors_event_t sensor_boom()
+{
+  /* Get a new sensor event */
+  sensors_event_t event; 
+  boom_mma.getEvent(&event);
+  return event;
+}
+
+sensors_event_t sensor_stick()
+{
+  /* Get a new sensor event */
+  sensors_event_t event; 
+  stick_mma.getEvent(&event);
+  return event;
+}
+
+void loop()
+{
+  unsigned char cmd[3];
+  unsigned char bytes_read = Serial.readBytes(cmd, 3);
+  sensors_event_t event;
+
+  if ( bytes_read )
+  {
+    switch ( cmd[0] )
+    {
+      case 0x01:
+        fire(BOOM_1, BOOM_2, cmd[1], cmd[2]);
+        break;
+      case 0x02:
+        fire(STICK_1, STICK_2, cmd[1], cmd[2]);
+        break;
+      case 0x03:
+        fire(BUCKET_1, BUCKET_2, cmd[1], cmd[2]); 
+        break;
+      case 0x04:
+        fire(HOUSE_1, HOUSE_2, cmd[1], cmd[2]);
+        break;
+      case 0x11:
+        event = sensor_boom();
+        Serial.println(event.acceleration.y);
+        break;
+      case 0x12:
+        event = sensor_stick();
+        Serial.println(event.acceleration.y);
+        break;
+    }
+  }
+
+  delay(100);
+}
